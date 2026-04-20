f5f5b06ddd48729b86f5a7dbe915b67b36a3f975
Addison Blais <calvinseikoblais@gmail.com>
2026-04-19 23:33:20 -0400

    fuck uh i fortog
    - separated hypr files into shared and profile, for configs i'll only
    want on main hyprland setup vs for caelestia setup
    - did a bunch of other stuff

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -5,4 +5,11 @@ abbr -a c code
 abbr -a spot spotify_player
 abbr -a hyprconf "micro ~/projects/dotfiles/hypr/hyprland.conf"
 abbr -a yay paru
+abbr -a matrix "unimatrix -fa -s 95 -c magenta"
 # git functions
+abbr -a ga. "git add ."
+abbr -a ga "git add"
+abbr -a gp "git push"
+abbr -a gpl "git pull"
+abbr -a gc "git commit"
+abbr -a gcm "git commit -m"
