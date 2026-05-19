c43c842ac155b7b03668f20b3e5bc9aef927c08b
addiGoat <calvinseikoblais@gmail.com>
2026-05-18 23:40:26 -0400

    restructured/cleaned up rofi scripts

--- hypr/dot-config/hypr/modules/rofi.lua
+++ hypr/dot-config/hypr/modules/rofi.lua
@@ -4,4 +4,11 @@ local function bindRofi(super, key, name)
 	hl.bind(super .. " + " .. key, hl.dsp.exec_cmd(rofi_script_dir .. "/rofi-" .. name))
 end
 
+-- Website launcher
 bindRofi("ALT", "SHIFT + L", "launch")
+
+-- Notes
+bindRofi("ALT", "SHIFT + N", "notes")
+
+-- Power
+bindRofi("ALT", "SHIFT + X", "power")
