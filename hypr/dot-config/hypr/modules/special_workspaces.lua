3c7fe5432f1d8d1756aed61dbc95a57980cb4509
addiGoat <calvinseikoblais@gmail.com>
2026-05-26 22:08:29 -0400

    added tmux configs, changed discord to vesktop, modified kitty 'cyan' color

--- hypr/dot-config/hypr/modules/special_workspaces.lua
+++ hypr/dot-config/hypr/modules/special_workspaces.lua
@@ -1,6 +1,6 @@
 local util = require("modules.utils")
 local mainMod = "ALT"
 
-util.make_special(mainMod, "SHIFT + D", "discord", "discord", "discord")
+util.make_special(mainMod, "SHIFT + D", "vesktop", "vesktop", "vesktop")
 util.make_special(mainMod, "GRAVE", "kitty", "scratchpad", "kitty --class scratchpad")
 
