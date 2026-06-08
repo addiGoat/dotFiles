a92927711e88a9c2b5ccf56b39d5ed9e302aeae0
addiGoat <calvinseikoblais@gmail.com>
2026-06-08 00:01:09 -0400

    Added some lsp binds, move live server binds to module

--- nvim/dot-config/nvim/lua/config/binds.lua
+++ nvim/dot-config/nvim/lua/config/binds.lua
@@ -2,13 +2,6 @@ local map = function(mode, key, cmd, desc)
     vim.keymap.set(mode, key, cmd, { desc = desc })
 end
 
-vim.keymap.set('n', '<leader>lss', function()
-    vim.cmd("LiveServerStart")
-end)
-
-vim.keymap.set('n', '<leader>lsx', function()
-    vim.cmd("LiveServerStop")
-end)
 
 map('n', '<leader>w', '<cmd>w<CR>', "Save File")
 map('n', '<leader>a', '<cmd>wa<CR>', "Save All")
