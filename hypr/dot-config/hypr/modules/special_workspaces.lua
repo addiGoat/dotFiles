225caa283fe14dda00b057423d2620c01532d479
addiGoat <calvinseikoblais@gmail.com>
2026-07-02 01:03:27 -0400

    added scratchpad workspace - removed kitty workspae

--- hypr/dot-config/hypr/modules/special_workspaces.lua
+++ hypr/dot-config/hypr/modules/special_workspaces.lua
@@ -2,5 +2,3 @@ local util = require("modules.utils")
 local mainMod = "ALT"
 
 util.make_special(mainMod, "SHIFT + D", "vesktop", "vesktop", "vesktop")
-util.make_special(mainMod, "GRAVE", "kitty", "scratchpad", "kitty --class scratchpad")
-
