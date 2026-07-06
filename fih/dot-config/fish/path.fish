ed1596fbf60e345a66fe1b8dff3b27cdc302d5b3
addiGoat <calvinseikoblais@gmail.com>
2026-07-05 21:15:59 -0400

    separate fish path and vars to own files

---
+++ fih/dot-config/fish/path.fish
@@ -0,0 +1,4 @@
+fish_add_path /home/addigoat/.spicetify
+fish_add_path /home/addigoat/scripts
+fish_add_path /home/addigoat/.cargo/bin
+fish_add_path /home/addigoat/BuildFiles/tiled/default/install-root/usr/local/bin/
