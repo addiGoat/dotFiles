8c90f1c5805bf9a043c890f0a151b549b900a50f
addiGoat <calvinseikoblais@gmail.com>
2026-07-07 21:50:55 -0400

    kristal logger

--- fih/dot-config/fish/conf.d/aliases.fish
+++ fih/dot-config/fish/conf.d/aliases.fish
@@ -22,3 +22,4 @@ alias lt 'eza -aT --color=auto --group-directories-first --icons --ignore-glob="
 alias grep='grep --color=auto'
 
 abbr kristal '/usr/bin/love /home/addigoat/Projects/Engines/Kristal/'
+abbr kristail tail -f ~/Projects/Engines/Kristal/kristal.log
