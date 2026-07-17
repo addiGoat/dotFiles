b2636fee9c8457ff26fbb9b23f70edb16f8a7a6f
addiGoat <calvinseikoblais@gmail.com>
2026-07-17 01:17:41 -0400

    some fish stuff\

--- fih/dot-config/fish/conf.d/aliases.fish
+++ fih/dot-config/fish/conf.d/aliases.fish
@@ -12,14 +12,20 @@ abbr pyenv "source .venv/bin/activate.fish"
 abbr tailget "sudo tailscale file get /home/addigoat/Taildrop/"
 abbr fixlock "sudo systemctl restart sddm"
 
-# Replace ls with eza
+alias grep='grep --color=auto'
+
+abbr kristal '/usr/bin/love /home/addigoat/Projects/Engines/Kristal/'
+abbr kristail tail -f ~/Projects/Engines/Kristal/kristal.log
+
+## ---- replace builtins ----
+
+# ls with eza
 alias ls "eza -l --color=auto --group-directories-first --icons"
 alias la "eza -la --color=auto --group-directories-first --icons"
 alias lsg "eza -lg --color=auto --group-directories-first --icons"
 alias lag "eza -lag --color=auto --group-directories-first --icons"
 alias lt 'eza -aT --color=auto --group-directories-first --icons --ignore-glob=".git|.venv"'
 
-alias grep='grep --color=auto'
+# cat with bat
 
-abbr kristal '/usr/bin/love /home/addigoat/Projects/Engines/Kristal/'
-abbr kristail tail -f ~/Projects/Engines/Kristal/kristal.log
+abbr b bat
--- fih/dot-config/fish/path.fish
+++ fih/dot-config/fish/path.fish
@@ -2,3 +2,4 @@ fish_add_path /home/addigoat/.spicetify
 fish_add_path /home/addigoat/scripts
 fish_add_path /home/addigoat/.cargo/bin
 fish_add_path /home/addigoat/BuildFiles/tiled/default/install-root/usr/local/bin/
+fish_add_path /home/addigoat/Projects/Engines/
