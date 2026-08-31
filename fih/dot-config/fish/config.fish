d4f5726abd9ec5abbfa8e2fc76cfd889fde7f1ce
addiGoat <calvinseikoblais@gmail.com>
2026-08-31 15:15:29 -0400

    add ssh agent to fish

--- fih/dot-config/fish/config.fish
+++ fih/dot-config/fish/config.fish
@@ -2,6 +2,9 @@ source ~/.config/fish/conf.d/done.fish
 source ~/.config/fish/path.fish
 source ~/.config/fish/var.fish
 
+# SSH Agent
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
 ## Config
 # format man pages
 set -x MANROFFOPT "-c"
