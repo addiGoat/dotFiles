d41d8b8340cc43e5cff3931c32fb4a117c027197
addiGoat <calvinseikoblais@gmail.com>
2026-09-02 03:01:21 -0400

    change size and pos of pavucontrol utility

--- hypr/dot-config/hypr/modules/window_rules/exceptions.lua
+++ hypr/dot-config/hypr/modules/window_rules/exceptions.lua
@@ -13,7 +13,7 @@ hl.window_rule({
     opaque = true,
     border_color = "#5d78b0",
     monitor = "DP-1",
-    move = {1675, 50},
-    size = {875, 460},
+    move = {1675, 45},
+    size = {865, 700},
     ["hyprbars:no_bar"] = true
 })
