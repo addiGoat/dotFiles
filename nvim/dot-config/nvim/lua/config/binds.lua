80db5bde6674b939c00c2b6de5f40591e0d71a76
addiGoat <calvinseikoblais@gmail.com>
2026-09-03 21:59:45 -0400

    att W, Wq, WQ, and Q commands to nvim for common typos

--- nvim/dot-config/nvim/lua/config/binds.lua
+++ nvim/dot-config/nvim/lua/config/binds.lua
@@ -11,6 +11,11 @@ map('n', '<leader>x', '<cmd>wqa<CR>', "Save All & Quit")
 map('n', '<leader>qq', '<cmd>q!<CR>', "Quit Without Saving")
 map('n', '<leader>qa', '<cmd>qa!<CR>', "Quit all buffers Without Saving")
 
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("WQ", "wq", {})
+vim.api.nvim_create_user_command("Q", "q", {})
+
 
 -- Escape function switch
 -- if search is highlighted, escape will unhighlight
