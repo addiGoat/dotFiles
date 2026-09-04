2c4098206c71f0fafe52112a26fb7ca5fba8ae67
addiGoat <calvinseikoblais@gmail.com>
2026-09-04 19:41:45 -0400

    install autopair for fish

--- fih/dot-config/fish/path.fish
+++ fih/dot-config/fish/path.fish
@@ -1,4 +1,3 @@
-fish_add_path /home/addigoat/.spicetify
 fish_add_path /home/addigoat/scripts
 fish_add_path /home/addigoat/.cargo/bin
 fish_add_path /home/addigoat/BuildFiles/tiled/default/install-root/usr/local/bin/
