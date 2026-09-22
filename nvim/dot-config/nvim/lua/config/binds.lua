4782a7a9c37330135cee3dbea655818baf15d5c7
addiGoat <calvinseikoblais@gmail.com>
2026-09-22 01:09:24 -0400

    align binds

--- nvim/dot-config/nvim/lua/config/binds.lua
+++ nvim/dot-config/nvim/lua/config/binds.lua
@@ -5,16 +5,20 @@ end
 
 -- General operation commands
 
-map('n', '<leader>w', '<cmd>w<CR>', "Save Current")
-map('n', '<leader>a', '<cmd>wa<CR>', "Save All")
-map('n', '<leader>x', '<cmd>wqa<CR>', "Save All & Quit")
-map('n', '<leader>qq', '<cmd>q!<CR>', "Quit Without Saving")
-map('n', '<leader>qa', '<cmd>qa!<CR>', "Quit all buffers Without Saving")
+--  mode, keybind,      command,        description
+map('n',  '<leader>w',  '<cmd>w<CR>',   "Save Current")
+map('n',  '<leader>a',  '<cmd>wa<CR>',  "Save All")
+map('n',  '<leader>x',  '<cmd>wqa<CR>', "Save All & Quit")
+map('n',  '<leader>qq', '<cmd>q!<CR>',  "Quit Without Saving")
+map('n',  '<leader>qa', '<cmd>qa!<CR>', "Quit all buffers Without Saving")
 
-vim.api.nvim_create_user_command("W", "w", {})
-vim.api.nvim_create_user_command("Wq", "wq", {})
-vim.api.nvim_create_user_command("WQ", "wq", {})
-vim.api.nvim_create_user_command("Q", "q", {})
+map('n',  '<C-:>',      ':!',           "Run external shell command")
+
+vim.api.nvim_create_user_command("W",    "w",    {})
+vim.api.nvim_create_user_command("Q",    "q",    {})
+vim.api.nvim_create_user_command("Wq",   "wq",   {})
+vim.api.nvim_create_user_command("WQ",   "wq",   {})
+vim.api.nvim_create_user_command("Wqa",  "wqa",  {})
 
 
 -- Escape function switch
